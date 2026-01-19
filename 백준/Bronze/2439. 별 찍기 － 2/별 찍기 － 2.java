8e91831875df2610ba00e4643deb13b0d662c862
alskung1101 <162026230+alskung1101@users.noreply.github.com>
2026-01-19 09:15:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args)throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+
+            for (int h= 1; h <= N - i; h++) {
+                System.out.print(" ");
+            }
+            for (int h = 1; h <= i; h++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
