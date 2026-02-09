9506093d079bab836d630a0cf45e68cacf6a0f21
alskung1101 <162026230+alskung1101@users.noreply.github.com>
2026-02-09 10:36:10 +0900

    [level 1] Title: 폰켓몬, Time: 4.74 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set <Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        if(nums.length/2<=set.size()){
+            answer=nums.length/2;
+        }else{
+            answer=set.size();
+        }
+        return answer;
+    }
+}
